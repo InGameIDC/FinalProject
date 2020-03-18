59c28e9b0ef2f4c85fc7e8434053133e73f17044
ilan12 <ilanscode@gmail.com>
2020-03-18 19:08:10 +0200

    First Game setting
    Added Temp Hero,
    Added Temp Terrain.
    Added HeroUnit Class,
    Added Skill Class,
    
    Implement Movement Func

---
+++ Assets/Scripts/HeroUnit.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HeroUnit : MonoBehaviour
+{
+
+    float DESIRED_POS_MARGIN_OF_ERROR = 0.1f;  // needs to finel.
+    float RESPAWN_TIME;                // needs to final.
+
+    Action <HeroUnit> onMove;        //
+    Action <HeroUnit> onHit;        // handles hero hit ( health > 0)
+    Action <HeroUnit> onRespawn;   //
+    Action <HeroUnit> onDeath;    // handles hero death (0 >= health)
+
+    int id;
+    float currentHeatlh;
+    float maxHealth; 
+    // skill SKILL;
+    float moveSpeed;
+    List <HeroUnit> herosToAttackBank; 
+    HeroUnit heroToAttack;      // this is the target to be attacked by the hero.
+    Vector3 desiredPos;        // the location desired to move to.
+    HeroUnit targetHero;      // the selected enemy to be attacked.
+    enum status {dead,siege,moving,idle,attacking,moveAndAttack};
+
+    private void Awake()
+    {
+        desiredPos = Vector3.zero;
+        moveSpeed = 2f;
+    }
+
+    void Start()
+    {
+       
+    }
+
+   
+    void Update()
+    {
+        movmentLite();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void movmentLite()   // 
+    {
+        Vector3 direction = this.desiredPos - this.transform.position;
+        direction =  direction.normalized;
+        rotateTowardDirection(this.desiredPos);
+
+        if (Vector3.Distance((this.transform.position), this.desiredPos) > DESIRED_POS_MARGIN_OF_ERROR)
+        {
+            this.transform.position += direction * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// Rotate the target toward the desired direction
+    /// ###### NEED TO BE CHANGE, TO CHECK IF THE ROTATION REQUIRE ######
+    /// </summary>
+    private void rotateTowardDirection(Vector3 target)
+    {
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = target - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = moveSpeed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+}
---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    float id;
+    float dammageMultiplier;
+    float damage;
+    float range;
+    float attackSpeed;
+    float coolDown; 
+    // projectile PROJECTILE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
