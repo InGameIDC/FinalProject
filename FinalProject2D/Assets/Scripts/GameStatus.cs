1aebf97452c18c5225680ae1940e88aac84e825b
orstein9 <62151984+orstein9@users.noreply.github.com>
2020-04-25 13:05:17 +0300

    with updated main menu

---
+++ FinalProject2D/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    public float currentXP = 0f;
+    public int coins = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Load Data from playerprefs
+        currentXP = PlayerPrefs.GetFloat("currentXP", 0);
+        coins = PlayerPrefs.GetInt("coins", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        PlayerPrefs.SetFloat("currentXP", currentXP);
+        PlayerPrefs.SetInt("coins", coins);
+    }
+
+}
---
+++ FinalProject2D/Assets/Scripts/MainMenu/LevelSelection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+
+    [SerializeField] private bool unlocked; // Default value is false
+    public Image lockImage;
+
+    private void Start()
+    {
+        UpdateLevelImage();
+    }
+
+    private void UpdateLevelImage()
+    {
+        if (!unlocked)
+        {
+            lockImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            lockImage.gameObject.SetActive(false);
+        }
+    }
+
+    public void PressSelection(string levelName)
+    {
+        if (unlocked)
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+
+}
