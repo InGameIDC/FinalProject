c3e529e0be0ea3827b5f2c5fc45a6ded532c01b8
Or Daniel <sunor4@gmail.com>
2020-05-13 13:20:47 +0300

    Addition of PrjectileExplosion

---
+++ FinalProject2D/Assets/Scripts/ProjectileExplosion.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ProjectileExplosion : MonoBehaviour
+{
+    // public Action<Projectile, Collider2D> onHitMechs; // for data updates
+    // public Action<Projectile> onHitDisplayers; // for feedbakcs: visual and audio displays.
+    public GameObject attacker;
+
+    private float _hitRadius = 3f;
+    private string _enemyTag = "Enemy";
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        Collider[] enemiesInSphere = Physics.OverlapSphere(target.gameObject.transform.position, _hitRadius);
+        foreach (Collider enemy in enemiesInSphere)
+        {
+            if (enemy.CompareTag(_enemyTag))
+            {
+                //Damage the enemy
+                if (TeamTool.isEnemy(attacker, target.gameObject))
+                {
+                    //on hitting - the health is lowered
+
+                    GetComponent<CircleCollider2D>().isTrigger =
+                        false; // turn off the trigger (can't use the same bullet twice)
+
+                    if (onHitMechs != null)
+                        onHitMechs(this, target);
+
+                }
+            }
+        }
+    }
+}
+
