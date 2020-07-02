af66a4cddf13bd0aa5004b186c2d449b244f0665
Or Daniel <sunor4@gmail.com>
2020-07-02 17:08:46 +0300

    Addition of TutManager

---
+++ FinalProject2D/Assets/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Tutorial : MonoBehaviour
+{
+    private int m_Id;
+    public string m_ExplanationText;
+
+    public int Id { get; set; }
+    public string ExplanationText { get; set; }
+
+    public virtual void CheckIfHappening()
+    {
+    }
+
+    void Awake()
+    {
+        TutorialManager.Instance.TutorialList.Add(this);
+    }
+}
