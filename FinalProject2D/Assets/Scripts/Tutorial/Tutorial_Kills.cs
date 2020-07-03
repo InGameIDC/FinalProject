7a04b00aaf89f0b264153650c022a0ee691c6395
ilan12 <ilanscode@gmail.com>
2020-07-03 11:17:40 +0300

    Added Tutorials_Kills

---
+++ FinalProject2D/Assets/Scripts/Tutorial/Tutorial_Kills.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Kills : Tutorial
+{
+    [SerializeField] public List<Health> TargetsToKill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Health taget in TargetsToKill)
+        {
+            taget.OnDeath += removeTarget;
+        }
+    }
+
+    private void removeTarget(GameObject target)
+    {
+        Health targetHealth = target.GetComponent<Health>();
+        TargetsToKill.Remove(targetHealth);
+        targetHealth.OnDeath -= removeTarget;
+
+        if (TargetsToKill.Count == 0 && TutorialManager.Instance.CurrentTutorial == this)
+            TutorialManager.Instance.CompletedTutorial();
+    }
+}
