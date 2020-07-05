b58ea308dff5fc3525efe65f2c49be578ae0def1
Dorpere <62248150+Dorpere@users.noreply.github.com>
2020-07-05 10:46:20 +0300

    1234

--- FinalProject2D/Assets/Scripts/Tutorial/tutorial_2.cs
+++ FinalProject2D/Assets/Scripts/Tutorial/tutorial_2.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tutorial_2 : MonoBehaviour
+public abstract class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public gameObject popUp;
+    [SerializeField]
+    private int m_Id;
+
+    [TextArea]
+    public string m_ExplanationText;
+    
+    public int Id { get; set; }
+    public string ExplanationText { get; set; }
+    string first = "it looks like your friends are prisoned" 
+    string second = "in order to free them you must fight the carrots";
+    string third = "to attack a vegtable u must touch your hero and then touch the vegtalbe u want to to attack."
+    string forth = "kill the carrots"
+
+    gameObject text_box.text
+    
+
+
+    public virtual void CheckIfHappening()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        TutorialManager.Instance.TutorialList.Add(this);
     }
 }
