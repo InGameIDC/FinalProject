e84edd2e278152a540a5207ecfe697e1893ff330
orstein9 <62151984+orstein9@users.noreply.github.com>
2020-07-07 22:45:30 +0300

    with some new mechanism to testOHAD

---
+++ FinalProject2D/Assets/AreaManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaManager : MonoBehaviour
+{
+    public GameObject ShootingButton;
+    public int numOfCurrentAreas;
+    public int numOfMaxAreas;
+    private bool isItMax = false;
+    public GameObject bm; //battle manager
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(numOfCurrentAreas == numOfMaxAreas && !isItMax)
+        {
+            isItMax = true;
+            ShootingButton.GetComponent<Button>().interactable = false;
+            bm.GetComponent<BattleManager>().onPressShooting();
+        }
+        else if(numOfCurrentAreas != numOfMaxAreas && isItMax)
+        {
+            isItMax = false;
+            ShootingButton.GetComponent<Button>().interactable = true;
+        }
+    }
+
+    public void AddedArea()
+    {
+        numOfCurrentAreas++;
+    }
+
+    public void RemovedArea()
+    {
+        numOfCurrentAreas--;
+    }
+}
--- FinalProject2D/Assets/Scripts/OHAD/AreaScript.cs
+++ FinalProject2D/Assets/Scripts/OHAD/AreaScript.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class AreaScript : MonoBehaviour
 {
     private List<GameObject> objectsInField;
+    public float _hitRadius;
+    public float shootDamege;
+    public int timeToDie;
+    public GameObject am; // AreaManager
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        objectsInField = new List<GameObject>();
+        am = GameObject.FindGameObjectWithTag("AreaManager");
+        am.GetComponent<AreaManager>().AddedArea();
+
+        Collider2D[] damageHitAreasInSphere;
+        damageHitAreasInSphere = Physics2D.OverlapCircleAll(transform.position, _hitRadius, LayerMask.GetMask("DamageHitArea"));
+        foreach (Collider2D damageHitArea in damageHitAreasInSphere)
+        {
+            GameObject unit = damageHitArea.transform.parent.gameObject;
+            //Damage the enemy
+            if (unit.tag == "EnemyUnit")
+            {
+                //on hitting - the health is lowered
+                unit.GetComponentInChildren<Health>().TakeDamage(shootDamege);
+
+                //GetComponent<Collider2D>().isTrigger = false; // turn off the trigger (can't use the same bullet twice)
+            }
+        }
+
+        StartCoroutine(waitToDestroy());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator waitToDestroy()
+    {
+
+        yield return new WaitForSeconds(timeToDie);
+
+        am.GetComponent<AreaManager>().RemovedArea();
+        Destroy(gameObject);
+
     }
 
 
@@ -27,7 +61,7 @@ public class AreaScript : MonoBehaviour
             GameObject targetParentUnit = unit.transform.parent.gameObject;
             unit = targetParentUnit;
             Debug.Log(unit.name);
-            //objectsInField.Add(unit);
+            objectsInField.Add(unit);
             unit.GetComponent<Movment2D>().ChangeSpeed(0.5f);
 
         }
@@ -41,10 +75,9 @@ public class AreaScript : MonoBehaviour
         {
             GameObject targetParentUnit = unit.transform.parent.gameObject;
             unit = targetParentUnit;
-            //objectsInField.Remove(unit);
+            objectsInField.Remove(unit);
             unit.GetComponent<Movment2D>().ChangeSpeed(1f);
         }
 
-        
     }
 }
