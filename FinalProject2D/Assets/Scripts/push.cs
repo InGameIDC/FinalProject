0f97827cc27e4fb64f759d2bbb3f57cc447e97cc
orstein9 <62151984+orstein9@users.noreply.github.com>
2020-08-12 23:52:48 +0300

    with Dors Changes

---
+++ FinalProject2D/Assets/Scripts/push.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class push : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Vector3 direction;
+    [SerializeField] float speed = 1f;
+    List<GameObject> targetOnBoard = new List<GameObject>();
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        PushTarget(other.gameObject);
+    }
+
+    private void PushTarget(GameObject target)
+    {
+        Transform rb = target.GetComponent<Transform>();
+        Vector3 currentPos = rb.position;
+        Vector3 newPos = currentPos + direction * Time.deltaTime;
+        rb.position = newPos;
+    }
+}
---
+++ FinalProject2D/Assets/Scripts/red_fill_animation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class red_fill_animation : MonoBehaviour
+{
+    private Animator a_Animator;
+    void Start()
+    {
+        a_Animator = gameObject.GetComponent<Animator>();
+        a_Animator.SetBool("loosing", false);
+    }
+    void update()
+    {
+        if (gameObject.GetComponentInChildren<Animator>().GetBool("loosing"))
+        {
+            a_Animator.SetBool("loosing", true);
+        }
+            a_Animator.SetBool("loosing", false);
+    }
+}
